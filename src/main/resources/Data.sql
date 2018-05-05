871c0f699dbdfb891212b7c4d53feb0533c8e319
Hrystos <krisimen@gmail.com>
2018-05-05 14:49:19 +0300

    issue #2 Store Villages

---
+++ src/main/resources/Data.sql
@@ -0,0 +1,10 @@
+INSERT INTO VILLAGE (name, latitude, longitude) VALUES ('Serres', 41.092083, 23.541016);
+INSERT INTO VILLAGE (name, latitude, longitude) VALUES ('Provatas', 41.068238, 23.390686);
+INSERT INTO VILLAGE (name, latitude, longitude) VALUES ('A. Kamila',  	41.058320, 23.424134);
+INSERT INTO VILLAGE (name, latitude, longitude) VALUES ('K. Kamila', 41.020431, 23.483293);
+INSERT INTO VILLAGE (name, latitude, longitude) VALUES ('K. Mitrousi', 41.058680, 23.457547);
+INSERT INTO VILLAGE (name, latitude, longitude) VALUES ('Koumaria', 41.016434, 23.434656);
+INSERT INTO VILLAGE (name, latitude, longitude) VALUES ('Skoutari', 41.020032, 23.520701);
+INSERT INTO VILLAGE (name, latitude, longitude) VALUES ('Adelfiko', 41.014645, 23.457354);
+INSERT INTO VILLAGE (name, latitude, longitude) VALUES ('Ag. Eleni', 41.003545, 23.559196);
+INSERT INTO VILLAGE (name, latitude, longitude) VALUES ('Peponia', 40.988154, 23.516756);
