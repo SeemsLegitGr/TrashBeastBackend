90a28a849a0ff1ea35c9ff44edac81bb24adaa32
Hrystos <krisimen@gmail.com>
2018-05-06 12:40:13 +0300

    semi final push

--- src/main/WebApp/populateDropdown.js
+++ src/main/WebApp/populateDropdown.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     $.ajax({
-        url: "localhost:8080/getAllVillages"
-    }).then(function(data) {
-        $.each(data, function (key, entry) {
-            $(.'menulist').append("<button type=\"button\" class=\"btn btn-primary\">" + .append(data.name) + "</button>");
-        })
-    });
+    url: "localhost:8080/getAllVillages"
+}).then(function(data) {
+    $.each(data, function (key, entry) {
+        $(.'menulist').append("<button type=\"button\" class=\"btn btn-primary\">" + .append(data.name) + "</button>");
+    })
+});
 });
